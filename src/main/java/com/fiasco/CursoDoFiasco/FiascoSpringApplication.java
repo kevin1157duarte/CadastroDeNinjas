ec29af5a2f334e3f0335dd7f999addbbeaddad13
kevin1157duarte <kevin1157.kd@gmail.com>
2025-05-06 22:01:40 -0300

    first commit

---
+++ src/main/java/com/fiasco/CursoDoFiasco/FiascoSpringApplication.java
@@ -0,0 +1,13 @@
+package com.fiasco.CursoDoFiasco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiascoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FiascoSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fiasco/CursoDoFiasco/FiascoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiasco.CursoDoFiasco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FiascoSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
